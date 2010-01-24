7fa1db943b6c84dbf593a7683b065fc04ce40b11
Travis Dent <tcdent@gmail.com>
2010-01-24 11:29:17 -0800

    Options are now passed as an array on instantiation, or by calling set_option afterword.

--- lib/Twitter.php
+++ lib/Twitter.php
@@ -6,7 +6,7 @@
  * 
  * @author Travis Dent <tcdent@gmail.com>
  * @copyright (c) 2009 Travis Dent.
- * @version 0.2.4
+ * @version 0.3
  * 
  * Public (unauthenticated) methods:
  * 
@@ -28,33 +28,56 @@
  * 
  * // Send a direct message.
  * $twitter->direct_messages->new(array('user' => 12345, 'text' => 'foo'));
+ * 
+ * // View your lists. (Unfortunately, Twitter has adopted an inconsistent way 
+ * // of exposing the list resources that doesn't quite fit our model.)
+ * $twitter->{'username'}->lists();
+ * 
+ * 
+ * Retrieving results in another format and setting the user agent string:
+ * 
+ * $twitter = new Twitter('username', 'password', array(
+ *     'format' => "xml", 
+ *     'user_agent' => "my-application/0.1"
+ * ));
+ * 
+ * -or-
+ * 
+ * $twitter = new Twitter;
+ * $twitter->set_option('format', "XML");
+ * $twitter->set_option('user_agent', "my-application/0.1");
+ * 
  */
 
 class Twitter {
     
     public static $VERSION = 1;
     
-    public $user_agent = "php-twitter/0.2.4";
-    
     private $user;
     private $pass;
-    private $format;
     private $uri;
+    private $options;
     
-    public function __construct($user=FALSE, $pass=FALSE, $format='json', $user_agent=NULL, $uri=NULL){
-        if(!in_array($format, array('json', 'xml', 'rss', 'atom')))
-            throw new TwitterException("Unsupported format: $format");
-        
+    public function __construct($user=FALSE, $pass=FALSE, $options=array(), $uri=NULL){
         $this->user = $user;
         $this->pass = $pass;
         $this->uri = $uri;
-        $this->format = $format;
-        if($user_agent !== NULL) $this->user_agent = $user_agent;
+        
+        $this->options = array_merge(array(
+            'user_agent' => "php-twitter/0.3", 
+            'format' => 'json'
+        ), $options);
+        
+        if(!in_array($this->options['format'], array('json', 'xml', 'rss', 'atom')))
+            throw new TwitterException("Unsupported format: ".$this->options['format']);
+    }
+    
+    public function set_option($key, $value){
+        return $this->options[$key] = $value;
     }
     
     public function __get($key){
-        return new Twitter(
-            $this->user, $this->pass, $this->format, $this->user_agent, $key);
+        return new Twitter($this->user, $this->pass, $this->options, $key);
     }
     
     public function __call($method, $args){
@@ -64,16 +87,14 @@ class Twitter {
             CURLOPT_RETURNTRANSFER => TRUE, 
             // Twitter returns a HTTP code 417 if we send an expectation.
             CURLOPT_HTTPHEADER => array('Expect:'),
-            CURLOPT_HEADER => 0
+            CURLOPT_HEADER => 0, 
+            CURLOPT_USERAGENT => $this->options['user_agent']
         );
         
         if($this->user && $this->pass){
             $curlopt[CURLOPT_USERPWD] = sprintf("%s:%s", $this->user, $this->pass);
             $curlopt[CURLOPT_HTTPAUTH] = CURLAUTH_BASIC;
         }
-        
-        if($this->user_agent)
-            $curlopt[CURLOPT_USERAGENT] = $this->user_agent;
         
         $uri = ($this->uri)? sprintf("%s/%s", $this->uri, $method) : $method;
         
@@ -83,11 +104,12 @@ class Twitter {
         }
         
         if($method == 'search'){
-            $url = sprintf("search.twitter.com/%s.%s", $uri, $this->format);
+            $url = sprintf("search.twitter.com/%s.%s", 
+                $uri, $this->options['format']);
         }
         else {
             $url = sprintf("api.twitter.com/%d/%s.%s", 
-                self::$VERSION, $uri, $this->format);
+                self::$VERSION, $uri, $this->options['format']);
         }
         
         if(in_array($method, array('new', 'create', 'update', 'destroy'))){
@@ -107,7 +129,7 @@ class Twitter {
         if($meta['http_code'] != 200)
             throw new TwitterResponseException($meta, $data);
         
-        if($this->format == 'json')
+        if($this->options['format'] == 'json')
             return json_decode($data);
         
         return $data;
@@ -118,20 +140,21 @@ class TwitterException extends Exception {}
 
 class TwitterResponseException extends TwitterException {
     
-    public $http_code;
+    public $response;
+    public $data;
     
     public function __construct($response, $data){
-        $this->http_code = $response['http_code'];
+        $this->response = $response;
         
         $message = sprintf("Response code %d from %s", 
-            $response['http_code'], $response['url']);
+            $this->response['http_code'], $this->response['url']);
         
-        if(strpos($response['content_type'], "json")){
-            $data = json_decode($data);
-            $message .= " - ".$data->error;
+        if(strpos($this->response['content_type'], "json")){
+            $this->data = json_decode($data);
+            $message .= " - ".$this->data->error;
         }
         
-        parent::__construct($message, $this->http_code);
+        parent::__construct($message, $this->response['http_code']);
     }
 }
 
